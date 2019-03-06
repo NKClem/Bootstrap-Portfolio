da0f8e872dc2fbc006f59a25e70cabba14df18db
Nicole K. Clements <nkclem09@gmail.com>
2019-03-05 21:10:45 -0500

    adjusted css for portfolio page

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,26 +2,33 @@ var portfolioItems = [
     {
         title: "Magical Harry Potter Trivia Game",
         image: "./assets/images/hp-trivia.png",
-        summary: "Harry Potter-themed trivia game created with JavaScript.",
+        summary: "Harry Potter-themed trivia game, created with JavaScript.",
         url: "https://nkclem.github.io/Magical_Harry_Potter_Trivia/"
     },
     {
         title: "Train Time",
         image: "./assets/images/train-time.png",
-        summary: "Train schedule app, using Firebase as the backend.",
+        summary: "Train schedule app, using Firebase for the backend.",
         url: "https://nkclem.github.io/Train_Schedule_App/"
     },
     {
         title: "Eat. Dat. Burger!",
         image: "./assets/images/burger-app.png",
-        summary: "Burger creation/logging app, using MySQL as the back-end and HandlebarsJS as the front-end.",
+        summary: "Burger creation/logging app, using MySQL for the back-end and HandlebarsJS for the front-end.",
         url: "https://sleepy-mesa-52132.herokuapp.com"
+    },
+    {
+        title: "RVA Mag Scraper",
+        image: "./assets/images/rva_scrape.jpg",
+        summary: "Current news taken from RVA Magazine's website, using MongoDB for the back-end.",
+        url: "https://damp-island-70228.herokuapp.com/articles"
     }
 ]
 
 function buildPortfolio() {
+    $('#portfolioItems').empty();
     portfolioItems.forEach(function(element) {
-        $('#portfolioItems').append('<div class="card"><img src=" '+element.image+' "/><div class="card-body"><h5 class="card-title">' + element.title + '</h5><p class="card-text">' +element.summary+ '</p><a href=" '+element.url+' " target="_blank">See App</a></div></div>');
+        $('#portfolioItems').append('<div class="card portfolioCard"><img class="img-fluid" src=" '+element.image+' "/><div class="card-body"><h5 class="card-title">' + element.title + '</h5><p class="card-text">' +element.summary+ '</p><a href=" '+element.url+' " target="_blank">See App</a></div></div>');
     });
 }
 
